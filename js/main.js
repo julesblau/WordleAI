8def9925b3d7f769f8a53533c7ed9489413d0fda
jmayrides <jmayride@villanova.edu>
2022-09-15 17:31:23 -0400

    Reading in words file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-var solution = 
+const fs = require("fs");
+
+var words;
+
+fs.readFile("resources/words.txt", (err, words) => {
+    if (err) throw err;
+    console.log(words.toString());
+});
+
+console.log(words.length);
+
+var solution = Math.random();
+
