ea535164debfb5daa7fca7987303b66d562b5d7f
julesblau <jblaust1@villanova.edu>
2022-10-14 17:07:11 -0600

    Stuck at this point. Last working code.

--- server.cjs
+++ server.cjs
@@ -1,4 +1,3 @@
-const fs = require('fs');
 //We use express for Browser to interpret HTML
 const express = require("express");
 const app = express();
@@ -9,21 +8,14 @@ const ls = spawn('python', ['scripts/Easy.py']);
 
 ls.stdout.on('data', (data) => { //EXPORT THIS TO SCRIPT.JS
     var aiGuess = data + '';
-   
 });
 
 ls.stderr.on('data', (data) => {
   console.log(`stderr: ${data}`);
 });
 
-//---Trying to send this over to script.js so we can use it for ai board, not working...
-// export function sendGuess()
-// {
-//     return aiGuess;
-// }
-
 //Launch server
-app.listen(8888, () => {
+app.listen(8889, () => {
   console.log("Application started and Listening on port 8888");
 });
 
