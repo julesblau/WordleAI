6ffcdeb90316a5e1b0fb02e26ce940fbe9698782
julesblau <jblaust1@villanova.edu>
2022-10-26 15:13:43 -0400

    Added pathJules to Easy.py

--- scripts/Easy.py
+++ scripts/Easy.py
@@ -2,11 +2,11 @@ import numpy as np
 import sys #so we can access arguments when passed. i.e. sys.argv[1], etc.
 
 pathJack = "/Users/jmayrides/git"
-pathJules = "/User/julesblau/Desktop"
+pathJules = "/Users/julesblau/Desktop"
 
 if __name__ == '__main__':
     #Read in WordList
-    _list = np.loadtxt(pathJack + "/WordleAI/resources/solutions.txt", dtype='str') #we're going to want to change this path when putting this is done.
+    _list = np.loadtxt(pathJules + "/WordleAI/resources/solutions.txt", dtype='str') #we're going to want to change this path when putting this is done.
     #Select random guess from solutions
     guess = np.random.choice(_list)
     #Return guess to server.js
