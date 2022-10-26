180a92743730c144cc5e2e639963d2bc92b766e5
jchandl3 <jchandl3@villanova.edu>
2022-10-26 16:44:36 -0400

    Updated home page

--- scripts/Easy.py
+++ scripts/Easy.py
@@ -7,7 +7,7 @@ pathJake = "/Applications/MAMP/htdocs"
 
 if __name__ == '__main__':
     #Read in WordList
-    _list = np.loadtxt(pathJack + "/WordleAI/resources/solutions.txt", dtype='str') #we're going to want to change this path when putting this is done.
+    _list = np.loadtxt(pathJake + "/WordleAI/resources/solutions.txt", dtype='str') #we're going to want to change this path when putting this is done.
     #Select random guess from solutions
     guess = np.random.choice(_list)
     #Return guess to server.js
