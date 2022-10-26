1e79c92f08cc4367131ce0538f026684dc79b630
jchandl3 <jchandl3@villanova.edu>
2022-10-26 15:27:09 -0400

    Jake path

--- scripts/Easy.py
+++ scripts/Easy.py
@@ -3,10 +3,11 @@ import sys #so we can access arguments when passed. i.e. sys.argv[1], etc.
 
 pathJack = "/Users/jmayrides/git"
 pathJules = "/Users/julesblau/Desktop"
+pathJake = "/Applications/MAMP/htdocs"
 
 if __name__ == '__main__':
     #Read in WordList
-    _list = np.loadtxt(pathJack + "/WordleAI/resources/solutions.txt", dtype='str') #we're going to want to change this path when putting this is done.
+    _list = np.loadtxt(pathJake + "/WordleAI/resources/solutions.txt", dtype='str') #we're going to want to change this path when putting this is done.
     #Select random guess from solutions
     guess = np.random.choice(_list)
     #Return guess to server.js
