5d3da3e7f19f52e5bf57875554e966ca7bfe83a7
jmayrides <jmayride@villanova.edu>
2022-10-26 15:57:04 -0400

    Refactor python

--- server.js
+++ server.js
@@ -3,27 +3,13 @@ const express = require("express");
 const app = express();
 
 //Call Easy.py to recieve First  guess
-const spawn = require('child_process').spawn;
-const ls = spawn('python', ['scripts/Easy.py']);
 var aiGuess;
-ls.stdout.on('data', (data) => { 
-  aiGuess = data + '';
-})
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
+getGuess()
 
 //Set endpoint for sending python data. Collect Ensuing Guesses
 app.get('/py-data', (req, res) => {
-  const spawn = require('child_process').spawn;
-  const ls = spawn('python', ['scripts/Easy.py']);
-  ls.stdout.on('data', (data) => { 
-    aiGuess = data + '';
-  })
-  ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  });
-    res.send(aiGuess);
+  getGuess();
+  res.send(aiGuess);
 })
 
 //Launch server
@@ -33,3 +19,14 @@ app.listen(8889, () => {
 
 //Avoid MIME type checking from browser for boards
 app.use(express.static(__dirname));
+
+function getGuess() {
+  const spawn = require('child_process').spawn;
+  const ls = spawn('python3', ['scripts/Easy.py']);
+  ls.stdout.on('data', (data) => {
+    aiGuess = data + '';
+  });
+  ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+}
