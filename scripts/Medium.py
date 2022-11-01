a11bca1f1e47bf99ef6840d2d2890477490a60d4
julesblau <jblaust1@villanova.edu>
2022-10-31 21:42:10 -0400

    Added comments to Medium on how to proceed

--- scripts/Medium.py
+++ scripts/Medium.py
@@ -10,7 +10,15 @@ pathJake = "/Applications/MAMP/htdocs"
 if __name__ == '__main__':
     #Read in WordList
     _list = np.loadtxt(pathJules + "/WordleAI/resources/solutions.txt", dtype='str') 
-    # Map is {Letter Position(1-5): A-Z}
+    # Create Map to store possible letters
     possible_letters = dict.fromkeys(range(1,6), list(string.ascii_lowercase))
-    print(possible_letters)
+    
+    #If we recieve gray, remove that letter from all positions in map
+    #If we recieve yellow, remove that letter from that position in map
+    #If we recieve green, remove all other letters from that position in map
+
+    #CASE TO CONSIDER:
+    #Guess is "guess" and one 's' is yellow and one is gray
+    # Maybe a set of yellow letters that need to be in the final word??
+    
     
