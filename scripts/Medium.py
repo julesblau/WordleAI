8a198f9b332f3943ee3c0bf1588f355ba7888337
julesblau <jblaust1@villanova.edu>
2022-10-31 21:28:03 -0400

    Update map so letter positions are 1-5

--- scripts/Medium.py
+++ scripts/Medium.py
@@ -10,6 +10,7 @@ pathJake = "/Applications/MAMP/htdocs"
 if __name__ == '__main__':
     #Read in WordList
     _list = np.loadtxt(pathJules + "/WordleAI/resources/solutions.txt", dtype='str') 
-    # Map is {Letter Position(0-4): A-Z}
-    possible_letters = dict.fromkeys(range(5), list(string.ascii_lowercase))
+    # Map is {Letter Position(1-5): A-Z}
+    possible_letters = dict.fromkeys(range(1,6), list(string.ascii_lowercase))
+    print(possible_letters)
     
