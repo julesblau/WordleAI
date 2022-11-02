9fd27d45aa674263e1bd06c5387cbce22f325b5d
julesblau <jblaust1@villanova.edu>
2022-11-02 13:47:26 -0400

    Added steps for Medium data endpoint

--- server.js
+++ server.js
@@ -2,24 +2,17 @@
 const express = require("express");
 const app = express();
 
-//Call Easy.py to recieve First  guess
+//Recieve random guess to start always
 var aiGuess;
 getGuessEasy()
 
-//Set endpoint for sending python data. Collect Ensuing Guesses
+//Set endpoint for sending python EASY MODE data. Collect Ensuing Guesses
 app.get('/py-data-easy', (req, res) => {
   getGuessEasy();
   res.send(aiGuess);
 })
 
-//Launch server
-app.listen(8889, () => {
-  console.log("Application started and Listening on port 8889");
-});
-
-//Avoid MIME type checking from browser for boards
-app.use(express.static(__dirname));
-
+//Get Easy Guess
 function getGuessEasy() {
   const spawn = require('child_process').spawn;
   const ls = spawn('python', ['scripts/Easy.py']);
@@ -30,3 +23,32 @@ function getGuessEasy() {
     console.log(`stderr: ${data}`);
   });
 }
+
+//Set endpoint for sending python MEDIUM MODE data. Collect Ensuing Guesses
+app.get('/py-data-medium', (req, res) => {
+  // getGuessMedium();
+  // res.send(aiGuess);
+})
+
+//Set endpoint to recieve data from Script for Python AI
+// app.post('')
+
+//Get Medium Guess
+// function getGuessMedium() {
+//   const spawn = require('child_process').spawn;
+//   const ls = spawn('python', ['scripts/Medium.py']);
+//   ls.stdout.on('data', (data) => {
+//     aiGuess = data + '';
+//   });
+//   ls.stderr.on('data', (data) => {
+//     console.log(`stderr: ${data}`);
+//   });
+// }
+
+//Launch server
+app.listen(8889, () => {
+  console.log("Application started and Listening on port 8889");
+});
+
+//Avoid MIME type checking from browser for boards
+app.use(express.static(__dirname));
