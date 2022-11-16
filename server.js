0c4d026717aef93288d8b812d90ddd99cb917604
jmayrides <jmayride@villanova.edu>
2022-11-16 14:37:05 -0500

    FIX MEDIUM FINALLY

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //We use express for Browser to interpret HTML
 const express = require("express")
+const { chmod } = require("fs")
 const app = express()
 app.use(express.json())
 
@@ -7,7 +8,8 @@ app.use(express.json())
 var context = undefined
 var aiGuess = undefined
 var guess = undefined
-getGuessEasy()
+const spawn = require('child_process').spawn
+// getGuessEasy()
 
 //Get Easy Guess
 function getGuessEasy() {
@@ -22,17 +24,26 @@ function getGuessEasy() {
 }
 
 //Get Medium Guess
-function getGuessMedium() {
-  const spawn = require('child_process').spawn
-  console.log(guess + " " + context)
-  const ls = spawn('python3', ['scripts/Medium.py', guess, context]) //Call the function with an arguments
-  ls.stdout.on('data', (data) => { 
-    aiGuess = data + ''
-  })
-  ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`)
-  })
-}
+  const getGuessMedium = (cmd, args) => {
+
+    return new Promise((resolve, reject) => {
+      try {
+
+        const runCommand = spawn(cmd, args)
+        runCommand.stdout.on('data', data => resolve(data + ''))
+
+      } catch(e) {
+
+        reject(e)
+
+      }
+
+    })
+  
+  }
+
+
+
 
 //Set endpoint for sending python EASY MODE data. Collect Ensuing Guesses
 app.get('/py-data-easy', (req, res) => {
@@ -41,10 +52,8 @@ app.get('/py-data-easy', (req, res) => {
 })
 
 //Set endpoint for sending python MEDIUM MODE data. Collect Ensuing Guesses
-app.get('/py-data-medium-get', (req, res) => {
-  getGuessMedium()
-  res.send(aiGuess)
-  aiGuess = undefined
+app.get('/py-data-medium-get', async (req, res) => {
+  await getGuessMedium('python3', ['scripts/Medium.py', guess, context]).then((value) => { res.send(value) })
 })
 
 //Set endpoint to recieve data from Script for Python AI
