ef7bfa0c95bac5b80001d07f6eaf4bcb101376fd
jmayrides <jmayride@villanova.edu>
2022-11-21 16:55:46 -0500

    Add duplicate checking to medium and hard

--- scripts/Hard.py
+++ scripts/Hard.py
@@ -7,10 +7,13 @@ if __name__ == '__main__':
     wordListFile = open("resources/solutions.txt", "r")
     wordList = wordListFile.read().split("\n")
 
+    output = open("scripts/outputtext.txt", "w")
+
     # Create lists to hold context of letters
     greens = [None, None, None, None, None]
     yellows = [[], [], [], [], []]
     grays = []
+    duplicates = []
 
     # Get guess history and context from system args
     guessHistory = sys.argv[1].strip("[]").split(",")
@@ -29,12 +32,14 @@ if __name__ == '__main__':
                         yellows[k].remove(currLetter)
                 if currLetter in grays:
                     grays.remove(currLetter)
+                    duplicates.append(currLetter)
 
             # If context is 1, add letter to yellow list in known incorrect position if not already there
             elif (context[i])[j] == '1' and currLetter not in yellows[j] and currLetter not in greens:
                 yellows[j].append(currLetter)
                 if currLetter in grays:
                     grays.remove(currLetter)
+                    duplicates.append(currLetter)
 
             # If context is 0, add letter to gray list if not already there
             elif (context[i])[j] == '0' and currLetter not in grays and currLetter not in greens:
@@ -44,6 +49,11 @@ if __name__ == '__main__':
                         add = False
                 if add:
                     grays.append(currLetter)
+            elif (context[i])[j] == '0' and currLetter in greens:
+                duplicates.append(currLetter)
+
+    output.write(','.join(duplicates))
+    output.write('\n')
 
     possibleGuesses = wordList.copy()
 
@@ -77,12 +87,25 @@ if __name__ == '__main__':
                 possibleGuesses.remove(word)
                 break
 
+    wordList = possibleGuesses.copy()
+
+    for word in wordList:
+        for letter in duplicates:
+            if word.count(letter) > 1:
+                possibleGuesses.remove(word)
+                output.write(word)
+                output.write('\n')
+                break
+
+    output.write(','.join(possibleGuesses))
+
     # Choose random word from remaining (valid) words
     guess = np.random.choice(possibleGuesses)
     print(guess)
 
     wordList = wordListFile.read().split("\n")
     wordListFile.close()
+    output.close()
     possibleGuesses = wordList.copy()
     
     sys.stdout.flush()
